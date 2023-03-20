dd4ce241c77b18f36fe5b8c41d34b9010553f330
NANOCODES18 <harrigold.18@gmail.com>
2023-03-20 12:05:37 +0100

    google translate addedd

--- resources/views/layouts/customlayout.blade.php
+++ resources/views/layouts/customlayout.blade.php
@@ -69,6 +69,21 @@
                             <span>EN</span> </a>
                             <!-- /dropdown link -->
 
+                            <!--google translate -->
+
+                            <div id="google_translate_element"></div>
+
+                            <script type="text/javascript">
+                                function googleTranslateElementInit() {
+                                  new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
+                                }
+                                </script>
+
+                                <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+                                
+                            <!--google translate -->
+
+
                             <!-- Dropdown Option -->
                             <div class="dropdown-menu" x-placement="bottom-start" style="position: absolute; transform: translate3d(8px, 72px, 0px); top: 0px; left: 0px; will-change: transform;">
                                                                 <button class="dropdown-item sitelangChange" type="button" data-id="switchlang/English">
